9e3c4923fffa3571aadb6bda429e283b1f497099
richparks92 <richparks92@gmail.com>
2023-04-06 10:20:29 -0700

    Added timestamp, fixed sort order

--- src/components/invocationsList.js
+++ src/components/invocationsList.js
@@ -1,5 +1,5 @@
 import 'primeflex/primeflex.css';
-import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
+import { DataView } from 'primereact/dataview';
 import { Tree } from 'primereact/tree';
 import 'primeicons/primeicons.css';
 import { getTreeNodesFromInvocation } from '../scripts/util/helpers';
@@ -8,15 +8,12 @@ import { getTreeNodesFromInvocation } from '../scripts/util/helpers';
 //The outer row will separate function name/time called and the tree table
 const dataViewItemTemplate = (invocationRecord) => {
     const treeNodes = getTreeNodesFromInvocation(invocationRecord.callArgs);
-    // console.log('nodes:')
-    // console.log(treeNodes)
-
     return (
         <div className="col-12">
-            <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
+            <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-2">
 
-                <div className="text-lg font-bold">{invocationRecord.fnPath}</div>
-                <div className="text-base font-bold text-500">Timestamp: {invocationRecord.timestamp}</div>
+                <div className="text-base font-bold">{invocationRecord.fnPath}</div>
+                <div className="text-base text-500">{new Date(invocationRecord.timestamp).toString()}</div>
                 <Tree value={treeNodes}
                     nodeTemplate={treeNodeTemplate}>
                 </Tree>
@@ -26,14 +23,12 @@ const dataViewItemTemplate = (invocationRecord) => {
 };
 
 const treeNodeTemplate = (node) => {
-    //let label = <b>{node.label}</b>;
     let label = ''
     if (node.data && node.data.objKey) {
         label = <div><span>{node.data.objKey}: </span><span style={{ color: 'blue' }}>{node.data.objVal}</span></div>;
     } else {
         label = <span>{node.label}</span>
     }
-
     return label;
 }
 
@@ -42,11 +37,21 @@ format of invocation record
 {type: 'FUNCTION_CALL_EVENT', callArgs: args, timestamp: Date.now(), fnPath: '${fnPath}'}*/
 export default function InvocationsList2(props) {
     console.log(`Props: ${JSON.stringify(props)}`)
-    const invocationRecords = props.invocationRecords.map((invocationRecord) => {
-        //invocationRecord.callArgs = JSON.parse(invocationRecord.callArgs)
+    const invocationRecords = props.invocationRecords.map((invocationRecord, index) => {
+        invocationRecord._index = index
         return invocationRecord
     })
     return (
-        <DataView value={invocationRecords} itemTemplate={dataViewItemTemplate} sortOrder={-1}></DataView>
+        <div className = 'flex flex-column xl:flex-row xl:align-items-start p-4 gap-2'>
+            <div className='text-xl font-bold'>Function Invocations</div>
+        <DataView value={invocationRecords} 
+        itemTemplate={dataViewItemTemplate} 
+        sortField = '_index' 
+        sortOrder={-1}
+        emptyMessage = "No recorded invocations yet!"
+        >
+
+        </DataView>
+        </div>
     )
 }
