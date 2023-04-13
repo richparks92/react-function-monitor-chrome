986e426041928339cd3e9be83262c6e6607126b5
richparks92 <richparks92@gmail.com>
2023-04-12 23:32:31 -0700

    working except inv and suggestions

--- public/chrome-pages/background.js
+++ public/chrome-pages/background.js
@@ -8,31 +8,33 @@ console.log('Background page starting.')
 
 // Background page -- background.js
 chrome.runtime.onConnect.addListener(function (port) {
-console.log('Background page-- runtime connect listener triggered. Port: ', port)
+    //if (port.name == 'devtools-page')...
+    console.log('Background page-- runtime connect listener triggered. Port: ', port)
     // assign the listener function to a variable so we can remove it later
-    var devToolsListener = async function (message, port) {
+    const devToolsListener = async function (message, port) {
         console.log('devToolsConnection', message, 'port', port)
         switch (message.type) {
             case "REQUEST_INJECTION":
                 try {
                     const res = await injectScript(message.scriptKey, message.tabId, message.args)
                     console.log('Background - Success injecting:', message.scriptKey)
-                    port.postMessage({type: 'INJECT_RESULT', scriptKey: message.scriptKey, injectSuccessful: res, tabId: message.tabId})
+                    port.postMessage({ type: 'INJECT_RESULT', scriptKey: message.scriptKey, injectSuccessful: res, tabId: message.tabId })
                 }
                 catch (e) {
                     console.log(e)
                 }
         }
-        
+
     }
-    // add the listener
-    port.onMessage.addListener(devToolsListener);
+
 
     const onCompletedHandler = async function (details) {
+        console.log('Window loaded. Details: ', details)
         if (details.url?.startsWith('chrome://')) return undefined
         if (details.parentFrameId == -1 && details.tabId) {
+
             try {
-                 res = await port.postMessage({ type: 'WINDOW_LOADED', tabId: details.tabId })
+                port.postMessage({ type: 'WINDOW_LOADED', tabId: details.tabId })
 
             } catch (e) {
                 console.log(e)
@@ -41,10 +43,12 @@ console.log('Background page-- runtime connect listener triggered. Port: ', port
     }
 
     const onBeforeNavigateHandler = async function (details) {
+        console.log('Window about to navigate. Details: ', details)
         if (details.url?.startsWith('chrome://')) return undefined
         if (details.parentFrameId == -1 && details.tabId) {
+
             try {
-                await port.postMessage({ type: 'BEFORE_NAVIGATE', tabId: details.tabId })
+                port.postMessage({ type: 'BEFORE_NAVIGATE', tabId: details.tabId })
             } catch (e) {
                 console.log(e)
             }
@@ -52,10 +56,13 @@ console.log('Background page-- runtime connect listener triggered. Port: ', port
     }
 
 
-    if(port.name == 'devToolsConnection'){
+    if (port.name == 'devtools-page') {
+        // add the listener
+        port.onMessage.addListener(devToolsListener);
         chrome.webNavigation.onCompleted.addListener(onCompletedHandler)
         chrome.webNavigation.onBeforeNavigate.addListener(onBeforeNavigateHandler)
         port.onDisconnect.addListener(function () {
+            console.log('Background.js -- port disconnected, removing listener.')
             port.onMessage.removeListener(devToolsListener);
         });
     }
