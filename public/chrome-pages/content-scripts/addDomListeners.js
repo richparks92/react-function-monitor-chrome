c2a9690e0b11d32a9537589ba91fe4704f226da5
richparks92 <richparks92@gmail.com>
2023-04-14 20:40:09 -0700

    Added addDomListeners check

--- public/chrome-pages/content-scripts/addDomListeners.js
+++ public/chrome-pages/content-scripts/addDomListeners.js
@@ -1,19 +1,25 @@
 console.log('Function call listener contentscript started.')
 
-const port = chrome.runtime.connect({ name: "dom-listeners" });
-port.onMessage.addListener(function (msg) {
-  console.log('Port message received in content script.', msg)
-});
+function addDomListeners() {
+
+  let domListenerPort
+  if (domListenerPort) return
+  domListenerPort = chrome.runtime.connect({ name: "dom-listeners" });
+  domListenerPort.onMessage.addListener(function (message) {
+    console.log('Port message received in content script.', message)
+  });
 
 
-    window.addEventListener("message", (event) => {
-      if (chrome?.runtime) {
-        if (event.data.type == 'FUNCTION_CALL_EVENT') {
-          port.postMessage({ type: 'FUNCTION_CALL_DETAILS', data: event.data })
-    
-        } else if (event.data.type == 'FN_ARRAY_READY_EVENT') {
-          port.postMessage({ type: 'FN_ARRAY_RESULT', fnArray: event.data.fnArray})
-        }
+  window.addEventListener("message", (event) => {
+    if (chrome?.runtime) {
+      if (event.data.type == 'FUNCTION_CALL_EVENT') {
+        domListenerPort.postMessage({ type: 'FUNCTION_CALL_DETAILS', data: event.data })
+
+      } else if (event.data.type == 'FN_ARRAY_READY_EVENT') {
+        domListenerPort.postMessage({ type: 'FN_ARRAY_RESULT', fnArray: event.data.fnArray })
       }
-    }, false)
+    }
+  }, false)
+}
+addDomListeners()
 
